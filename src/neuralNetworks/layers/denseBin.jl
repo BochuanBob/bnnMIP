fddd24b0558611774ea2c6327b6014eda8916bd3
BochuanBob <lub@rose-hulman.edu>
2020-05-08 15:12:02 -0500

    Rename dense.jl to denseBin.jl and Update the file.

---
+++ src/neuralNetworks/layers/denseBin.jl
@@ -0,0 +1,145 @@
+include("layerSetup.jl")
+export denseBin
+
+# A fully connected layer with sign() or
+# without activation function.
+function denseBin(m::JuMP.Model, x::VarOrAff,
+               weights::Array{T, 2}, bias::Array{U, 1};
+               takeSign=false, cuts=true) where {T<:Real, U<:Real}
+    if (~checkWeights(weights))
+        error("Each entry of weights must be -1, 0, 1.")
+    end
+    initNN!(m)
+    count = m.ext[:NN].count
+    m.ext[:NN].count += 1
+    (yLen, xLen) = size(weights)
+    if (length(bias) != yLen)
+        error("The sizes of weights and bias don't match!")
+    end
+    y = @variable(m, [1:yLen], lower_bound=-1, upper_bound=1,
+                base_name="y_$count")
+    if (takeSign)
+        for i in 1:yLen
+            neuronSign!(m, x, y[i], i, weights[i, :], bias[i], cuts=cuts)
+        end
+    else
+        @constraint(m, [i=1:yLen], y[i] ==
+                    bias[i] + sum(weights[i,j] * x[j] for j in 1:xLen))
+    end
+    return y
+end
+
+# Checking each entry of weights must be in -1, 0, 1.
+function checkWeights(weights::Array{T, 2}) where {T <: Real}
+    for weight in weights
+        if (~(weight in [-1, 0, 1]))
+            return false
+        end
+    end
+    return true
+end
+
+# A MIP formulation for a single neuron.
+# If cuts == false, it is a Big-M formulation.
+# Otherwise, cuts for the ideal formulation are added to the model.
+# index is only used to name the variable.
+function neuronSign!(m::JuMP.Model, x::VarOrAff, yi::VarOrAff, index::Int64,
+                weightVec::Array{T, 1}, b::U;
+                cuts=False) where {T<:Real, U<:Real}
+    # initNN!(m)
+    oneIndices = findall(weightVec .== 1)
+    negOneIndices = findall(weightVec .== -1)
+    nonzeroNum = length(oneIndices) + length(negOneIndices)
+    tau, kappa = getTauAndKappa(nonzeroNum, b)
+    Iset1 = union(oneIndices, negOneIndices)
+    addBNNCutCons!(m, x, yi, Iset1, oneIndices, negOneIndices, tau, kappa)
+    Iset2 = Array{Int64, 1}([])
+    addBNNCutCons!(m, x, yi, Iset2, oneIndices, negOneIndices, tau, kappa)
+    # TODO: Implement the callback function and cuts.
+    return nothing
+end
+
+# Add two constraint with given I, I^+, I^-, tau, kappa as shown
+# in Proposition 3.
+function addBNNCutCons!(m::JuMP.Model, x::VarOrAff, yi::VarOrAff,
+                            Iset::Array{Int64, 1},
+                            oneIndices::Array{Int64, 1},
+                            negOneIndices::Array{Int64, 1},
+                            tau::Int64, kappa::In64)
+    Ipos = intersect(Iset, oneIndices)
+    Ineg = intersect(Iset, negOneIndices)
+    lenI = length(Iset)
+    nonzeroNum = length(oneIndices) + length(negOneIndices)
+    @constraint(m, sum(x[i] for i in Ipos) - sum(x[i] for i in Ineg) >=
+                ((lenI - nonzeroNum - tau) * (1 + yi) /2) -
+                (1 - yi) * lenI/2)
+    @constraint(m, sum(x[i] for i in Ipos) - sum(x[i] for i in Ineg) <=
+                ((1 + yi) * lenI/2) -
+                (lenI - nonzeroNum + kappa) * (1 - yi) /2)
+    return nothing
+end
+
+# When w^T x + kappa <= 0, sign(w^T x + b) = -1.
+# When w^T x + tau >= 0, sign(w^T x + b) = 1.
+# Each entry of w must be in -1, 0, 1.
+# Return: tau, kappa.
+function getTauAndKappa(nonzeroNum::Int64, b::U) where {U<:Real}
+    if (floor(b) == ceil(b))
+        return getTauAndKappaInt(nonzeroNum, Int64(b))
+    else
+        return getTauAndKappaFloat(nonzeroNum, Float64(b))
+    end
+end
+
+# Get tau and kappa when b is an integer.
+# Return: tau, kappa.
+function getTauAndKappaInt(nonzeroNum::Int64, b::Int64)
+    tau = Inf
+    kappa = Inf
+    if (mod(b, 2) == 0)
+        if(mod(nonzeroNum, 2) == 0)
+            tau = b
+            kappa = b + 2
+        else
+            tau = b - 1
+            kappa = b + 1
+        end
+    else
+        if(mod(nonzeroNum, 2) == 0)
+            tau = b - 1
+            kappa = b + 1
+        else
+            tau = b
+            kappa = b + 2
+        end
+    end
+    return Int64(tau), Int64(kappa)
+end
+
+# Get tau and kappa when b is a real number but not an integer.
+# Return: tau, kappa.
+function getTauAndKappaFloat64(nonzeroNum::Int64, b::Float64)
+    tau = Inf
+    kappa = Inf
+    if (floor(b) == ceil(b))
+        error("b can not be an integer!")
+    end
+    if (mod(b, 2) == 0)
+        if(mod(nonzeroNum, 2) == 0)
+            tau = floor(b)
+            kappa = ceil(b) + 1
+        else
+            tau = floor(b) - 1
+            kappa = ceil(b)
+        end
+    else
+        if(mod(nonzeroNum, 2) == 0)
+            tau = floor(b) - 1
+            kappa = ceil(b)
+        else
+            tau = floor(b)
+            kappa = ceil(b) + 1
+        end
+    end
+    return Int64(tau), Int64(kappa)
+end
