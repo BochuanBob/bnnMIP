9218826a66f2da428d1727b7d5b2d39615ba76f7
BochuanBob <lub@rose-hulman.edu>
2020-06-25 11:29:17 -0500

    Added my_callback_value() for speedup.

---
+++ src/neuralNetworks/callbacks.jl
@@ -0,0 +1,14 @@
+function my_callback_value(opt::Gurobi.Optimizer,
+            cb_data::Gurobi.CallbackData, x::VariableRef)
+    return my_get(
+        opt, MOI.CallbackVariablePrimal(cb_data), index(x)
+    )
+end
+
+function my_get(
+    opt::Gurobi.Optimizer,
+    ::MOI.CallbackVariablePrimal{CallbackData},
+    x::MOI.VariableIndex
+)
+    return opt.callback_variable_primal[Gurobi._info(opt, x).column]::Float64
+end
